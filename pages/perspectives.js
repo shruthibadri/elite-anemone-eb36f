f750f14accce7b9e3cc259b7ba2cfa1694b0ed1d
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 12:38:25 +0000

    perspectives.js: changed

--- pages/perspectives.js
+++ pages/perspectives.js
@@ -22,7 +22,7 @@ export default function Perspectives({ posts, initialDisplayPosts, pagination })
     <>
       <PageSEO
         title={`Perspectives - ${siteMetadata.author}`}
-        description={siteMetadata.description} 
+        description={siteMetadata.description}
       />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
