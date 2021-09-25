2e0e9c64bc911052875a2f3f8c4977d903480266
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 08:05:41 +0000

    perspectives.js: changed

--- pages/perspectives.js
+++ pages/perspectives.js
@@ -38,7 +38,7 @@ export default function Perspectives({ posts, initialDisplayPosts, pagination })
                 title={d.title}
                 description={d.summary}
                 imgSrc={d.imgSrc}
-                href={`/perspectives/${d.slug}`}
+                href={''}
               />
             ))}
           </div>
