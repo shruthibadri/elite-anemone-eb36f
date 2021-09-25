4b32bfd7905c4ca2c84510f4aa38baed20915bed
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 08:32:53 +0000

    siteMetadata.js: changed

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'Tails Azimuth',
-  headerTitle: 'Takshshila Research',
+  headerTitle: 'Takshashila Research',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
