56fce1d95d56e2d35b94dc8950249e1cf0495881
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 11:30:27 +0000

    Perspective.js: changed

--- layouts/Perspective.js
+++ layouts/Perspective.js
@@ -27,7 +27,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             </div>
           </header>
           <div
-            className="pb-8 divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 "
+            className="pb-8 divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 max-w-screen-2xl "
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:pb-0 xl:col-span-3 xl:row-span-2">
