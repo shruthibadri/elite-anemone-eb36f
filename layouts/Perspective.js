db9562bf627af486580920760ac15392a1251558
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 11:33:03 +0000

    Perspective.js: changed

--- layouts/Perspective.js
+++ layouts/Perspective.js
@@ -15,7 +15,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
       <BlogSEO url={`${siteMetadata.siteUrl}/perspectives/${frontMatter.slug}`} {...frontMatter} />
       <ScrollTopAndComment />
       <article>
-        <div className="py-4 space-y-1 text-left ">
+        <div className="max-w-4xl">
           <header>
             <div className="py-4 space-y-1 text-left">
               <div className="py-2">
