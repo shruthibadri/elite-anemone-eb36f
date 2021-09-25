561edbe92affb32e08cb92f20a06cf019e8054a3
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 09:24:19 +0000

    Perspective.js: changed

--- layouts/Perspective.js
+++ layouts/Perspective.js
@@ -16,7 +16,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
       <article>
         <div>
           <header>
-            <div className="pb-10 space-y-1 text-left border-b border-gray-200 dark:border-gray-700">
+            <div className="pb-10  space-y-1 text-left border-b border-gray-200 dark:border-gray-700">
               <div>
                 <PageTitle>{title}</PageTitle>
               </div>
