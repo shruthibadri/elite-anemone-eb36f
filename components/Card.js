d7df9461d5ef32e3850011c88020a4c7012cc324
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 09:02:42 +0000

    Card.js: changed

--- components/Card.js
+++ components/Card.js
@@ -24,12 +24,7 @@ const Card = ({ title, description, imgSrc, href, tags }) => (
           height={306}
         />
       )}
-      <div className="p-6">
-        <div className="flex flex-wrap">
-        {tags.map((tag) => (
-            <Tag key={tag} text={tag} />
-        ))}
-        </div>        
+      <div className="p-6">       
         <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
           {href ? (
             <Link href={href} aria-label={`Link to ${title}`}>
