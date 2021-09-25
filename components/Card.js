90be419c5c8e99309bfcaf5eef1769f955a6558d
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 08:06:01 +0000

    Card.js: changed

--- components/Card.js
+++ components/Card.js
@@ -26,7 +26,7 @@ const Card = ({ title, description, imgSrc, href }) => (
       <div className="p-6">
         <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
           {href ? (
-            <Link href={`/perspectives/${slug}`} aria-label={`Link to ${title}`}>
+            <Link href={href} aria-label={`Link to ${title}`}>
               {title}
             </Link>
           ) : (
@@ -36,7 +36,7 @@ const Card = ({ title, description, imgSrc, href }) => (
         <p className="mb-3 prose text-gray-500 max-w-none dark:text-gray-400">{description}</p>
         {href && (
           <Link
-            href={`/blog/${slug}`}
+            href={href}
             className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label={`Link to ${title}`}
           >
