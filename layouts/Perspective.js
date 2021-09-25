20ba9ab07aecde97c319fcf5d73299368ee40799
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 11:50:53 +0000

    Perspective.js: changed

--- layouts/Perspective.js
+++ layouts/Perspective.js
@@ -25,7 +25,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                     <Tag key={tag} text={tag} />
                  ))}              
             </div>
-            <div className="bg-gray-100 p-6 my-4 font-light text-lg	"> 
+            <div className="bg-gray-100 p-6 my-4 font-light "> 
                 {summary} 
             </div>
           </header>
