fc665b7649dcc9ff2db09a076c39a49a173092d3
Stackbit Code Editor <projects@stackbit.com>
2021-09-25 06:53:31 +0000

    Card.js: changed

--- components/Card.js
+++ components/Card.js
@@ -6,23 +6,23 @@ const Card = ({ title, description, imgSrc, href }) => (
     <div className="h-full overflow-hidden border-2 border-gray-200 rounded-md border-opacity-60 dark:border-gray-700">
       {href ? (
         <Link href={href} aria-label={`Link to ${title}`}>
-          {/* <Image
+          <Image
             alt={title}
             src={imgSrc}
             className="object-cover object-center lg:h-48 md:h-36"
             width={544}
             height={306}
-          /> */}
+          />
         </Link>
-      ) : (<></>
-    //     <Image
-    //       alt={title}
-    //       src={imgSrc}
-    //       className="object-cover object-center lg:h-48 md:h-36"
-    //       width={544}
-    //       height={306}
-    //     />
-    //   )}
+      ) : (
+        <Image
+          alt={title}
+          src={imgSrc}
+          className="object-cover object-center lg:h-48 md:h-36"
+          width={544}
+          height={306}
+        />
+      )}
       <div className="p-6">
         <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
           {href ? (
